4b00cc4875e46be75acdcd5be58008edbac567a7
Olivernio <diegoozzysh@gmail.com>
2023-09-14 00:43:21 -0300

    Proyecto v1.0

---
+++ Proyecto/src/app/pages/correo/correo.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { Usuario } from 'src/app/model/usuario';
+
+@Component({
+  selector: 'app-correo',
+  templateUrl: './correo.page.html',
+  styleUrls: ['./correo.page.scss'],
+})
+export class CorreoPage implements OnInit {
+
+  public correo: string = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  public ingresarPaginaValidarRespuestaSecreta(): void {
+    const usuario = new Usuario('', '', '', '', '');
+    const usuarioEncontrado = usuario.buscarUsuarioValidoCorreo(this.correo);
+    if (!usuarioEncontrado) {
+      alert('El correo ingresado no se encuentra registrado en el sistema');
+    }
+    else {
+      const navigationExtras: NavigationExtras = {
+        state: {
+          usuario: usuarioEncontrado
+        }
+      };
+      this.router.navigate(['/pregunta'], navigationExtras);
+    }
+  }
+
+}
---
+++ Proyecto/src/app/pages/pregunta/pregunta.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from 'src/app/model/usuario';
+
+@Component({
+  selector: 'app-pregunta',
+  templateUrl: './pregunta.page.html',
+  styleUrls: ['./pregunta.page.scss'],
+})
+export class PreguntaPage implements OnInit {
+
+  public usuario: Usuario | undefined;
+  public respuesta: string = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      // Verificar si extras y state existen antes de acceder a sus propiedades
+      const state = this.router.getCurrentNavigation()?.extras?.state;
+      if (state) {
+        // Verificar si usuario existe antes de acceder a sus propiedades
+        if (state['usuario']) {
+          this.usuario = state['usuario'];
+        } else {
+          this.router.navigate(['/login']);
+        }
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  public PaginaValidarRespuestaSecreta(): void {
+    // Verificar si usuario existe antes de acceder a sus propiedades
+    if (this.usuario && this.usuario.respuestaSecreta === this.respuesta) {
+      alert('Tu respuesta es correcta: ' + this.usuario.password);
+    } else {
+      alert('Tu respuesta es incorrecta ');
+    }
+  }
+}
