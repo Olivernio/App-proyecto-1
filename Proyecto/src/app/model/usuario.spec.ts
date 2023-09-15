a7756aa19adde533c34685ec25460a28b21c55cd
Olivernio <diegoozzysh@gmail.com>
2023-09-15 02:26:53 -0300

    [Sustitución por el siguiente Commit]
    Esta es una prueba para una posible solución a la hora de ejecutar la APP dentro del GitHub.
    
    Co-Authored-By: Olivernio <57511524+Olivernio@users.noreply.github.com>
    Co-Authored-By: Martín Mondaca <108354063+XxMartinx@users.noreply.github.com>
    Co-Authored-By: msalinas28 <128086212+msalinas28@users.noreply.github.com>

--- Proyecto/src/app/model/usuario.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Usuario } from './usuario';
-
-describe('Usuario', () => {
-  it('should create an instance', () => {
-    // @ts-ignore: Ignora la advertencia de TypeScript
-    expect(new Usuario()).toBeTruthy();
-  });
-});
